732d77464e0f5c601a94978578bc61b1a05ec53d
KgradyClarkson <gradyks@clarkson.edu>
2024-01-22 14:50:53 -0500

    Touched up take_photo

--- Project1.py
+++ Project1.py
@@ -13,14 +13,15 @@ def main():
         engine.setProperty('volume', 0.5)
         voice_out("Hello welcome to the program, would you like to change the volume?")
         cvol = False #change volume
-        if voice_in:
+        if voice_in():
+            #change volume
             cvol = True
         while cvol == True:
             voice_out("what would you like the volume set to out of 100?")
             volume = voice_in
             engine.setProperty('volume', volume)
             voice_out("Is this volume good?")
-            if voice_in:
+            if voice_in():
                 cvol = False
         voice_out("Would you like to take a selfie or photograph an object?")
         voice_in()
@@ -51,6 +52,9 @@ def commands(vo_in):
             case "object":
                 cameramode = 1
                 take_photo()
+            case "change object": #repeat effect but I felt like we needed the option to change object.
+                cameramode = 1
+                take_photo() 
             case "position?":#I cant think of the correct word but like the framing of the object? Like centered and all that
                 voice_out("#Variable storing current object position") 
             case "yes":
@@ -69,24 +73,43 @@ def commands(vo_in):
                 return "tr"
             case _:
                 print("Nuh uh")
-def voice_in():
+def voice_in(object):
     #Start voice input
+    #code should not procceed until it gets an input
     vo_in =  "" #convert voice to string
-    return vo_in
+    if object:
+        return vo_in
+    return commands(vo_in)
 def voice_out(vo_out):
     engine.say(vo_out)
     engine.runAndWait()
 def take_photo():
-    voice_out("Ready for photo?")
-    #Take a little silly photo
     if cameramode == 0:
-        position = 1 #get position
-        voice_out("Your face is" + position + ". Would you like to change the position?")
-        if commands(voice_in()) == True:
+        voice_out("Are you ready for the selfie?")
+        if voice_in() == False:
+            voice_out("Say yes when ready")
+            voice_in()
+            #Take a little silly photo
+            #selfie
+            position = 1 #get position from api
+            voice_out("Your face is" + position + ". Would you like to change the position?")
+            if voice_in():
+                voice_out("What would you like the position to be")
+                reposition(voice_in())
+            else:
+                voice_out("Done")
+                voice_in()
+    else:
+        voice_out("What object would you like to photograph?")
+        object = voice_in(1)
+        position = 1 #get position from api
+        voice_out("The" + object + "is" + position + ". Would you like to change the position?")
+        if voice_in():
             voice_out("What would you like the position to be")
             reposition(voice_in())
         else:
             voice_out("Done")
+            voice_in()
 def reposition(posw):
     #Position wanted
     #Numbers are in no way shape or form final but this was my idea of how to interpret it
