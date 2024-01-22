84558f400ae3eab730f9f894f07ccecf464006ad
Megan <edwardmm@Clarkson.edu>
2024-01-22 17:18:43 -0500

    Added face detection and list of location data for face bounding box

--- Project1.py
+++ Project1.py
@@ -100,9 +100,23 @@ def take_photo():
     voice_out("Ready for photo?")
     
     #Take a little silly photo
-    result, image = webcam.read()
+    success, image = webcam.read()
 
     if cameramode == 0:
+        #Selfie option
+        image2 = cv.cvtColor(image,cv.COLOR_BGR2RGB) #changes color scale to allow mediapipe image processing
+        results = mp_face_detection.process(image) #uses face detection model to find faces in image
+        image2 = cv.cvtColor(image,cv.COLOR_RGB2BGR) #return image to original color scale
+
+        #draw face detection annotations 
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image2,detection) #all changes+annotations made to seperate image so clean version can be saved
+
+        #gets location of face
+        bbox = detection.location_date.relative_bounding_box
+        bbox_list = [bbox.xmin, bbox.ymin, bbox.width, bbox.height] #xmin and ymin are the coordiantes of the bottom left of the box
+
         position = 1 #get position
         voice_out("Your face is" + position + ". Would you like to change the position?")
         if commands(voice_in()) == True:
