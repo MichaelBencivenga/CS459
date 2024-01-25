246907d3716ef4475e733dc0d8839aef433f3359
Bench <michael.a.bencivenga@gmail.com>
2024-01-25 15:45:42 -0500

    Cleaned up and now returns text input as a string

--- Speech2Text.py
+++ Speech2Text.py
@@ -1,14 +1,12 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
 
-with sr.Microphone(sample_rate=16000) as source:
-    print("Start speaking now...")
-
-    while True:
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            print(text)
-        except:
-            print("Input not recorded, please try again.")
+def get_user_input():
+    r = sr.Recognizer()
+    with sr.Microphone(sample_rate=16000) as source:
+        print(
+            "Please state your command..."
+        )  # this can be changed to use text to speech for blind users
+        audio = r.listen(source, timeout=10, phrase_time_limit=3)
+        text = r.recognize_google(audio)
+        return text
